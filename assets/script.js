24fd1dfc5736e715dd5224ec5ccf5f982aeec4ac
expensiveglowstick <bradystafford4200@gmail.com>
2022-01-31 19:43:00 -0500

    seconds

--- assets/script.js
+++ assets/script.js
@@ -1,64 +1,73 @@
-var questions=[
+var questions = [
     {
-        quesion: "Inside which HTML element do we put the JavaScript tag?",
+        question: "Inside which HTML element do we put the JavaScript tag?",
         choices: ["<script>", "<link>", "<head>", "<section>"],
         answer: "<script>"
 
     },
     {
-        quesion: "Inside which HTML element do we put the JavaScript tag?",
+        question: "Inside which HTML element do we put the JavaScript tag?",
         choices: ["<script>", "<link>", "<head>", "<section>"],
         answer: "<script>"
 
+    },
+    {
+        question: "hello"
     }
+
 ];
 
-questions[0] =   {
-    quesion: "Inside which HTML element do we put the JavaScript tag?",
-    choices: ["<script>", "<link>", "<head>", "<section>"],
-    answer: "<script>"
-},
-questions[0].question
-questions[0].answer
-questions[0].choices[0]
-questions[0].choices[1]
+var questionsElement = document.querySelector(".questions")
+//     questions[0].question
+// questions[0].answer
+// questions[0].choices[0]
+// questions[0].choices[1]
 
-var correctAnswers = 0
-var wrongAnswers = 0
-var timeInterval = 15
+// var correctAnswers = 0
+// var wrongAnswers = 0
+// var timeInterval = 15
 
-                    0       1       2 
-var myArray = ["first", "second", "three"]
-myArray.length
-3
-myArray[0] = "first"
-myArray[1] = "second"
+// var myArray = ["first", "second", "three"]
+// myArray.length
+// 3
+// myArray[0] = "first"
+// myArray[1] = "second"
 
 
-function displayQuestion(){
-    for (var i=0; i < questions.length; i++) {
+function displayQuestion() {
+    for (var i = 0; i < questions.length; i++) {
         //creating divs
+
+        console.log(questions + "********" + questions[i].question)
+        var questionsDiv = document.createElement("div");
+        questionsDiv.innerText = questions[i].question;
+
+        questionsElement.appendChild(questionsDiv);
+
         //document.querySelector("div").appendingChild("newDiv")
-        if (questions[i].answer === userAnswer) {
-                correctAnswers++
+        // if (questions[i].answer === userAnswer) {
+        //         correctAnswers++
 
-        } else {
-            wrongAnswers++
-            timeInterval--
-        }
+        // } else {
+        //     wrongAnswers++
+        //     timeInterval--
+        // }
     }
-
-    function timeIntervalFunction() {
-        if (timeInterval === 0) {
-            stopGame()
-        }
-    }
-
     // create-append create html elements dynamically and appendChild them to a parent div
     // how to access hard coded html "doucment.querySelector(".class") document.querySelector("#id")
     // time interval - keep track of time and questions throughout the app - 15 second stopping time
     // local storage - saving the high score
-    
+
 }
+
+
+function timeIntervalFunction() {
+    if (timeInterval === 0) {
+        stopGame()
+    }
+}
+
+
+displayQuestion();
 
 
